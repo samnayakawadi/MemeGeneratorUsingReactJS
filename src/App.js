0d49fc49cb7da6944673125b543bfbd356c61f1c
Samir Nayakawadi <82933835+samnayakawadi@users.noreply.github.com>
2021-05-20 00:15:04 +0530

    Updated App.js Added export default to function App()

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Components/Header";
 import MemeGenerator from "./Components/MemeGenerator";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Header />
@@ -10,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
